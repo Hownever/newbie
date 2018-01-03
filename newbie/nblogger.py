a991762fa42cc6dd8e29314d4762585b6f4146c7
Hownever <498449937@qq.com>
2018-01-03 19:25:24 +0800

    logging

--- newbie/nblogger.py
+++ newbie/nblogger.py
@@ -24,7 +24,6 @@ from logging import handlers
 import threading
 
 log_relative_path = '{slash}'.format(slash="/").join(['', 'log', ''])
-print os.path.dirname(__file__) + log_relative_path
 
 
 def singleton(cls):
@@ -57,36 +56,43 @@ def singleton(cls):
 
 
 @singleton
-class VmtsLogger(object):
+class NbLogger(object):
     """
     Vmts logger wrap-class.
     """
 
     def __init__(self, name, base_dir=os.path.dirname(__file__) + log_relative_path):
-        self.formatter = '%(levelname)s - %(asctime)s %(name)s: %(message)s'
-        self.formmater_debug = '%(levelname)s - %(asctime)s %(name)s: %(message)s\n\tCall Stack Info:\n\t\tfunction: ' \
-                          '%(funcName)s\n\t\tmodule: %(module)s\n\t\tfile: %(pathname)s'
 
+        if not name:
+            raise ValueError("Missing name")
+
+        self.formatter = '%(levelname)s - %(asctime)s %(name)s: %(message)s'
+        self.formatter_debug = '%(levelname)s - %(asctime)s %(name)s: %(message)s\n\tCall Stack Info:\n\t\tfunction:' \
+                               '%(funcName)s\n\t\tmodule: %(module)s\n\t\tfile: %(pathname)s'
         self.pre_conf = True
         self.name = name
         self.fp = base_dir + name + '.log'
-        self.debug_mode = True
+        self.debug_mode = False
         self.level = logging.DEBUG if self.debug_mode else logging.INFO
-        self.logger = logging.getLogger()
+        self.logger = logging.getLogger(self.name)
         self.logger.setLevel(self.level)
-        self.formatter = self.formatter if not self.debug_mode else self.formmater_debug
+        self.formatter = self.formatter if not self.debug_mode else self.formatter_debug
 
         if not os.path.exists(base_dir):
             os.makedirs(base_dir)
+        if not self.logger.handlers:
+            self.file_handle = logging.handlers.RotatingFileHandler(self.fp, maxBytes=20480000,
+                                                                    backupCount=10, encoding='utf-8')
+            self.file_handle.setLevel(self.level)
+            self.file_handle.setFormatter(logging.Formatter(self.formatter))
+            self.console_handle = logging.StreamHandler()
+            self.console_handle.setLevel(self.level)
+            self.console_handle.setFormatter(logging.Formatter(self.formatter))
 
-        file_handle = logging.handlers.TimedRotatingFileHandler(self.fp, when='D', backupCount=5, encoding='utf-8')
-        file_handle.setLevel(self.level)
-        console_handle = logging.StreamHandler()
-        console_handle.setLevel(self.level)
-        console_handle.setFormatter(logging.Formatter(self.formatter))
-
-        self.logger.addHandler(file_handle)
-        self.logger.addHandler(console_handle)
+            # filter_condition = "18:12:42"
+            # file_handle.addFilter(logging.Filter(filter_condition))
+            self.logger.addHandler(self.file_handle)
+            self.logger.addHandler(self.console_handle)
 
     def debug(self, msg):
         """
@@ -133,6 +139,23 @@ class VmtsLogger(object):
 
         return self.logger.critical(msg)
 
+    def remove_handler(self):
+        """
+        remove handler
+        :return:
+        """
+        self.logger.removeHandler(self.console_handle)
+
 if __name__ == "__main__":
-    a = VmtsLogger("critical")
-    a.critical("jiashuo")
+    a = NbLogger("critical1")
+    a.critical("jiashuo_a")
+    print "-------------------"
+    b = NbLogger("critical2")
+    b.critical("jiashuo_b")
+    print "-------------------"
+    c = NbLogger("critical3")
+    c.critical("jiashuo_c")
+    print "-------------------"
+    d = NbLogger("critical4")
+    d.critical("jiashuo_d")
+    f = NbLogger("")
